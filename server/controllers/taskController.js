33bca930e4b96e96c89d1151588c182660d69c26
phantomjac <drchesireyang@gmail.com>
2025-07-28 10:07:43 +0300

    Feature: Contollers and middlewares

---
+++ server/controllers/taskController.js
@@ -0,0 +1,21 @@
+const Task = require("../models/Task");
+
+//POST /api/tasks
+exports.createTask = async (req,res) => {
+    const task = await Task.create({ ...req.body, owner: req.user.id});
+    res.json(task);
+};
+
+//GET /api/tasks/me
+exports.getMyTasks = async (req,res) => {
+    const tasks = await Task.find({ owner: req.user.id });
+    res.json(tasks);
+};
+
+//GET /api/tasks/all
+exports.getAllTasks = async (req,res) => {
+    const tasks = await Task.find().populate("owner", "email");
+    res.json(tasks);
+};
+
+//Left at 1:35:50
---
+++ server/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtokens");
+
+//checks token and sets req.user
+exports.protect = (req,res,next) => {
+    const auth = req.headers.authorization;
+    if(!auth || !auth.startsWith("Bearer ")) return res.status(401).json({message: "No token given"});
+
+    const token = auth.slit(" ")[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; //{id, role}
+        next();
+    }catch (error) {
+        return res.status(403).json({ message: "Invalid token"});
+    }
+};
+
+//checks role
+exports.authorize = (roles) => {
+    return (req,res,next) => {
+        if (!roles.includes(req.user.role)) return res.status(403).json({message: "Forbidden"});
+        next();
+    };
+};
