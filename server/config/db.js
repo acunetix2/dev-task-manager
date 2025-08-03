eb1a21dcd9de18d363d8a38fc21995e8056f598e
phantomjac <drchesireyang@gmail.com>
2025-08-03 16:21:45 +0300

    URI

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () =>  {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+			console.log("🔍 MONGO_URI:", process.env.MONGO_URI);
         });
         console.log("MongoDB connected successfully");  
     }catch (error) {
